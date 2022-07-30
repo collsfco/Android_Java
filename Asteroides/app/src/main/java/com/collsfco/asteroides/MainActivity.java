81020534233ed43f309a3e910c01567747f9022c
collsfco <collselectronicsprojects@gmail.com>
2022-07-30 17:45:18 +0200

    Contador land, Asteroides

---
+++ Asteroides/app/src/main/java/com/collsfco/asteroides/MainActivity.java
@@ -0,0 +1,21 @@
+package com.collsfco.asteroides;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void exitApp(View view){
+        finish();
+    }
+
+
+}
